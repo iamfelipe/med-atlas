42cc69c16497a464f60eb0d7bc6ff61744da57e2
iamfelipe <castillo.devsigner@icloud.com>
2025-03-09 10:46:19 -0500

    refactor(web): remove add patient button from patients page
    - Remove unused Button import from patients page
    - Simplify AppHeader component by removing action button
    - Clean up patient dashboard header rendering

--- apps/web/app/dashboard/patient/page.tsx
+++ apps/web/app/dashboard/patient/page.tsx
@@ -1,7 +1,6 @@
 import { AppHeader } from "@/components/app-header";
 import { columns } from "@/components/table-patients/patients.columns";
 import { DataTable } from "@/components/table-patients/patients.data-table";
-import { Button } from "@/components/ui/button";
 import { getAllPatients } from "@/server/user";
 
 export default async function Patient() {
@@ -9,9 +8,7 @@ export default async function Patient() {
 
   return (
     <>
-      <AppHeader title="Patients">
-        <Button>Add patient</Button>
-      </AppHeader>
+      <AppHeader title="Patients" />
       <DataTable columns={columns} data={patients} />
     </>
   );
